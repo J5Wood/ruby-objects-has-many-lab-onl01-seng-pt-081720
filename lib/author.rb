211d9f15eee4b8eeed101887809b41aa85c947d5
Jeremy Wood <js.wood206@gmail.com>
2020-09-11 04:19:34 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,6 @@
 class Author
 
-  attr_accessor :name
+  attr_accessor :name, :author
 
   def initialize(name)
     @name = name
@@ -13,6 +13,10 @@ class Author
   def add_post(post)
     post.artist = self
   end
+
+  def add_post_by_title(post)
+    post = Post.new(post)
+    post.author = self
 
 
 
