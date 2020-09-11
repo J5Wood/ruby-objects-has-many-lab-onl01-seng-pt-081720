61447986bd8879aaf4cd0ca7f6803882ea23e8a8
Jeremy Wood <js.wood206@gmail.com>
2020-09-11 04:06:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,14 +4,11 @@ class Artist
 
   def initialize(name)
     @name = name
-    @songs = []
-
   end
 
   def songs
     Song.all.select { |song| song.artist == self}
   end
-
 
   def add_song(song)
     song.artist = self
